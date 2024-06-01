04e2d9d5c9aed6138d9b40e2aa522c47888bf6c3
Akshay Bhamse <iamguddu37@gmail.com>
2024-06-01 13:38:03 +0530

    Initial Commit

---
+++ LearnLinux/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LearnLinux
+//
+//  Created by Akshay Bhasme on 01/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearnLinux/LearnLinuxApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnLinuxApp.swift
+//  LearnLinux
+//
+//  Created by Akshay Bhasme on 01/06/24.
+//
+
+import SwiftUI
+
+@main
+struct LearnLinuxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
